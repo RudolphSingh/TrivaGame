0bfa2ac065d23a796bd86eb08bdb042b23052bfb
RudolphSingh <145520153+RudolphSingh@users.noreply.github.com>
2024-11-10 22:42:19 -0500

    Add files via upload

---
+++ Triviagame--main/triviagame/triviagameApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct triviagameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OptionsView() // Replace ContentView with OptionsView
+        }
+    }
+}
+
